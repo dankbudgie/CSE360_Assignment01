e8d3fe97cd4502b9470167f5d9f1aa4732eb6a59
dankbudgie <60132730+dankbudgie@users.noreply.github.com>
2020-09-30 19:43:31 -0700

    added documentation

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -1,5 +1,15 @@
 package cse360assignment02;
 
+/**
+ * This is a defined package for CSE360 Assignment 01, on version control.
+ * The AddingMachine class includes methods that add or subtract values from a given total.
+ * You can also return a history of what has been added/subtracted.
+ *
+ * @author Zachary Her
+ * @version 1.0
+ * @since 2020-09-30
+ */
+
 public class AddingMachine {
     private int total = 0;
     String history = "0";
